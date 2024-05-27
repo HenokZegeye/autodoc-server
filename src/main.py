7e23a7ce6fff5e47a36f70ee330cd339f22ecd55
hbirru <henok.birru@nokia.com>
2024-05-27 16:04:24 +0300

    __pychached removed

--- src/main.py
+++ src/main.py
@@ -2,9 +2,26 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.config import set_llm_model, set_embed_model
 from dotenv import load_dotenv
+from contextlib import asynccontextmanager
+import os
 
 
-app = FastAPI()
+directories_to_create = [
+    "data/documentation",
+    "data/mr-changes",
+    "indexes/docs",
+    "indexes/mr-change-summary"
+]
+
+@asynccontextmanager
+async def lifespan(app):
+    for directory in directories_to_create:
+        os.makedirs(directory, exist_ok=True)
+        print(f"Directory {directory} created or already exists.")
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
 
 origins = ["*"]
 
@@ -19,5 +36,7 @@ app.add_middleware(
 load_dotenv()
 set_llm_model()
 set_embed_model()
+
+
 
 import src.routes
